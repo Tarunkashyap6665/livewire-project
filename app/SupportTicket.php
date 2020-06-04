a72313409e44762b1eb3cbd8163f3615419af39f
Tarun <tarunkashyap760@gmail.com>
2020-06-04 23:59:32 +0530

    Initial Release

--- app/SupportTicket.php
+++ app/SupportTicket.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class SupportTicket extends Model
 {
+    protected $guarded=[];
     public function comments(){
         return $this->hasMany(Comment::class);
     }
